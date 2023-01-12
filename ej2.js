03d8f1bc3f66953dbaa55f3220b2c8cb47f18fdb
martabelfon <martabelfon@gmail.com>
2023-01-12 19:57:42 +0100

    Iteracion 2 OK

--- ej2.js
+++ ej2.js
@@ -1,25 +1,27 @@
 // **Iteración #2: Buscar la palabra más larga**
 
-// Completa la función que tomando un array de strings como argumento devuelva el más largo, en caso de que
-//  dos strings tenga la misma longitud deberá devolver el primero.
+// Completa la función que tomando un array de strings como argumento devuelva el más largo, 
+// en caso de que dos strings tenga la misma longitud deberá devolver el primero.
 
 // Puedes usar este array para probar tu función:
 
-
 const avengers = ['Hulk', 'Thor', 'IronMan', 'Captain A.', 'Spiderman', 'Captain M.'];
 
+
+
+
 function findLongestWord(param) {
-   
-    
+
     let maxAvenger = param[0];
 
     for (let i = 1; i < param.length; i++) {
-        if (param[i].length > maxAvenger.length){
+        
+       if (param[i].length > maxAvenger.length) {
             maxAvenger = param[i];
-        }    
+       } 
+        
     }
-    return console.log(maxAvenger);
+    console.log(maxAvenger);
 }
 
 findLongestWord(avengers);
-
