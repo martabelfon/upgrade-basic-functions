f58f8c908c2bb24a14e8bf261696cfaf7e8f2cc7
martabelfon <martabelfon@gmail.com>
2023-01-13 15:14:12 +0100

    Iteracion 7 OK

--- ej7.js
+++ ej7.js
@@ -1,8 +1,8 @@
 // **Iteración #7: Buscador de nombres**
 
-// Crea una función que reciba por parámetro un array y el valor que desea comprobar que existe dentro 
-// de dicho array - comprueba si existe el elemento, en caso que existan nos devuelve la posición de dicho 
-// elemento y por la contra un false. Puedes usar este array para probar tu función:
+//? Crea una función que reciba por parámetro un array y el valor que desea comprobar que existe dentro 
+//? de dicho array - comprueba si existe el elemento, en caso que existan nos devuelve la posición de dicho 
+//? elemento y por la contra un false. Puedes usar este array para probar tu función:
 
 
 const nameFinder = [
@@ -18,6 +18,18 @@ const nameFinder = [
     'Jessica',
     'Marc'
   ];
-  function finderName(param) {
-    // insert code
+  
+  function finderName(param, buscarNombre) {
+    
+    for (let i = 0; i < nameFinder.length; i++) {
+     
+        if (nameFinder[i] === buscarNombre) {
+          console.log(i);
+        } else {
+          console.log(false);
+        }
+    }
   }
+
+  finderName(nameFinder, "Logan");
+  
