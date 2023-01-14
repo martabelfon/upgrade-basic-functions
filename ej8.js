e072ab263bf68edf8355c8a00dd0e677b0565dd1
martabelfon <martabelfon@gmail.com>
2023-01-14 10:44:16 +0100

    Iteracion 8 OK

--- ej8.js
+++ ej8.js
@@ -17,7 +17,20 @@ const counterWords = [
     'upgrade',
     'code'
   ];
+
+  // No acabo de entenderlo...
+  
   function repeatCounter(param) {
-    // insert code
+    let counter = {};
+    for (let i = 0; i < param.length; i++) {
+      if (param[i] in counter) {
+        counter[param[i]]++;
+      } else {
+        counter[param[i]] = 1;
+      }
+    }
+    return console.log(counter);
   }
+
+  repeatCounter(counterWords);
   
